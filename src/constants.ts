461921b822f224342cd38857b9f7380c9d689faa
André Masson <amwebexpert@gmail.com>
2026-01-08 12:28:53 -0600

    chore: poker planning implementation

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,7 @@ export const APP_VERSION_INFO = Object.freeze({
   AUTHOR: 'amwebexpert@gmail.com',
   VERSION: '4.0.0',
   VERSION_DATE: '2026-01-08',
-  VERSION_DATE_ISO: '2026-01-08T18:09:00.215Z',
+  VERSION_DATE_ISO: '2026-01-08T18:23:14.885Z',
 })
 
 export const LONG_VERSION_DATE = '4.0.0 (2026-01-08)'
